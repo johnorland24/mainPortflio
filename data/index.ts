13737ef16a72cd492dac5a7fcc67382c1b24e5f7
johnorland24 <johnorlandsudoy644@gmail.com>
2024-09-28 15:51:06 +0800

    update profile

--- data/index.ts
+++ data/index.ts
@@ -12,7 +12,7 @@ export const gridItems = [
     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
     imgClassName: "w-full h-full",
     titleClassName: "justify-end",
-    img: "/profile.jpg",
+    img: "/Profile.jpg",
     spareImg: "",
   },
   
